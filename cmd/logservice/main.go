075df8cc6447e6b752c8f748878047689efbcff1
hamzalsheikh <7.amza98@gmail.com>
2023-08-31 19:48:02 -0600

    first commit

---
+++ cmd/logservice/main.go
@@ -0,0 +1,34 @@
+package main
+
+//module grading
+//go 1.19
+
+import (
+	"context"
+	"fmt"
+	"grading/log"
+	"grading/registry"
+	"grading/service"
+	stlog "log"
+)
+
+func main() {
+	log.Run("./grading.log")
+
+	host, port := "localhost", "4000"
+	serviceAddr := fmt.Sprintf("http://%v:%v", host, port)
+
+	var reg registry.Registration
+	reg.ServiceName = registry.LogService
+	reg.ServiceURL = serviceAddr
+	reg.RequiredServices = make([]registry.ServiceName, 0)
+	reg.ServiceUpdateURL = reg.ServiceURL + "/services"
+	reg.HeartbeatURL = reg.ServiceURL + "/heartbeat"
+
+	ctx, err := service.Start(context.Background(), host, port, reg, log.RegisterHandlers)
+	if err != nil {
+		stlog.Fatal(err)
+	}
+	<-ctx.Done()
+	fmt.Println("Shutting down log service")
+}
