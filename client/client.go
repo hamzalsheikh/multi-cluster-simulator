6582704c20e2afe9d4f2148cf000c57c5d284a8a
hamzalsheikh <7.amza98@gmail.com>
2023-11-18 19:34:48 -0700

    client request cluster information from scheduler

--- client/client.go
+++ client/client.go
@@ -1,13 +1,39 @@
 package client
 
-import "sandbox/scheduler"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sandbox/scheduler"
+)
 
 type Client struct {
 	Name         string
 	Cluster      scheduler.Cluster
 	SchedulerURL string
+	maxJobMem    int
+	maxJobCore   int
 }
 
 func SetSchedURL(URL string) {
 	client.SchedulerURL = URL
+	client.newClient()
+}
+
+func (c *Client) newClient() {
+	// request cluster information & this can include more information
+	// in the future
+	res, err := http.Get(c.SchedulerURL + "/newClient")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var cluster scheduler.Cluster
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&cluster)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	c.Cluster = cluster
 }
