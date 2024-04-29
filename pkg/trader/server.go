8790673eeb5065ab21d3a6144ef47a3248d980dd
hamzalsheikh <7.amza98@gmail.com>
2024-04-28 21:35:29 -0600

    implements trader server (grpc)

--- pkg/trader/server.go
+++ pkg/trader/server.go
@@ -1,20 +1,71 @@
 package trader
 
 import (
-	"fmt"
-	"net/http"
+	"context"
+	"sync"
+	"time"
+
+	pb "github.com/hamzalsheikh/multi-cluster-simulator/pkg/trader/gen"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-// the client sends jobs to scheduler through http requests
-// client takes as an input a distribution and knows it's cluster size
-// the distribution dictates the job sizes to create and send to the scheduler
+type traderServer struct {
+	pb.UnimplementedTraderServer
 
-func RegisterHandlers() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, span := trader.Tracer.Start(r.Context(), "hello-span")
-		defer span.End()
-		fmt.Fprintf(w, "Hello!")
-	})
+	currentContract pb.ContractResponse
+	currLock        *sync.Mutex
+	id              uint32
 }
 
-// trader server communicates with scheduler and other clusters
+func NewTraderServer() *traderServer {
+	// initialize server
+	s := &traderServer{}
+	return s
+}
+
+func (s *traderServer) RequestResource(ctx context.Context, contract *pb.ContractRequest) (*pb.ContractResponse, error) {
+
+	// trade in progress, refuse to start new trader
+	s.currLock.Lock()
+	if s.currentContract.Id == 0 {
+		s.currLock.Unlock()
+		return &pb.ContractResponse{Approve: false}, nil
+	}
+	s.currLock.Unlock()
+	// consult policy
+	approve := trader.ApproveTrade(contract)
+
+	contractResponse := pb.ContractResponse{Id: s.id, Approve: approve}
+	s.id++
+
+	go func() {
+		time.Sleep(time.Second * 10)
+		// check if current response is still there, delete initialize
+		s.currLock.Lock()
+		defer s.currLock.Unlock()
+		if s.currentContract.Id != 0 {
+			s.currentContract = pb.ContractResponse{}
+		}
+	}()
+
+	// this will eventually be a stream for negotiation
+	return &contractResponse, nil
+}
+
+func (s *traderServer) ApproveContract(ctx context.Context, contract *pb.ContractResponse) (*pb.NodeObject, error) {
+	s.currLock.Lock()
+	defer s.currLock.Unlock()
+
+	c := s.currentContract
+	if c.Id != contract.Id {
+		return &pb.NodeObject{}, status.Errorf(codes.DeadlineExceeded, "Time limit exceeded, contract denied")
+	}
+
+	// contract valid, request resources from scheduler
+	virtualNode, err := getVirtualNode(trader.SchedulerClient, &pb.VirtualNodeRequest{Cores: contract.Cores, Memory: contract.Memory, Time: contract.Time})
+
+	// reinitialize currentContract for future activity
+	s.currentContract = pb.ContractResponse{}
+	return virtualNode, err
+}
